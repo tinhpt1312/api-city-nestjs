8699ae7b5591d1cecfe554b56e28cb9e88365c54
Pham The Tinh <phamthetinhcop1312@gmail.com>
2024-10-24 10:14:05 +0700

    firt commit

---
+++ src/config/db.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+
+@Injectable()
+export class PostgeresConfiguration implements TypeOrmOptionsFactory {
+  constructor(private readonly configService: ConfigService) {}
+
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'postgres',
+      host: this.configService.get('DB_HOST'),
+      port: +this.configService.get('DB_PORT'),
+      username: this.configService.get('DB_USERNAME'),
+      password: this.configService.get('DB_PASSWORD'),
+      database: this.configService.get('DB_DATABASE'),
+      entities: ['dist/**/entities/*, entity{.ts,.js}'],
+      subscribers: ['dist/**/subscr/*, entity{.ts,.js}'],
+      logging: true,
+      logger: 'advanced-console',
+    };
+  }
+}
